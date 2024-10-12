2bfba816b565ed6b52a4815548efdd061444ba0a
ImpacTusdevloper <krishkushwaha99999@gmail.com>
2024-10-12 21:54:09 +0530

    While loop and base

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SupportLib;
+
+//Var define
+int num = 101;
+
+SupportLib.ConsoleSupport.Separator();
+Console.ForegroundColor = ConsoleColor.Green;
+
+int i = 1;
+Console.WriteLine($"This is a table of {num}");
+while(i <= 20)
+{
+    Console.WriteLine(num * i);
+    i+=1;
+}
